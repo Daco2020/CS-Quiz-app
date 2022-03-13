b323aa20a781cb08455f5d289e86c3707944c9ec
Daco <dacokim32@gmail.com>
2022-03-13 22:02:13 +0900

    Feat: quiz-testcode

--- cs_quiz/apps/quiz/tests.py
+++ cs_quiz/apps/quiz/tests.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 
 
 class QuizViewTestCase(APITestCase):
-    url = reverse('cs_quiz.apps.quiz:quiz-list')
+    url = reverse('cs_quiz.apps.quiz:quiz-list') # 인자 중 args는 패스파라미터
     
     def setUp(self):
         self.category = Category.objects.create(content='네트워크', depth=0)
@@ -19,23 +19,32 @@ class QuizViewTestCase(APITestCase):
         res = self.client.get(self.url)
         self.assertEqual(200, res.status_code)
 
-        quiz_serializer_data = QuizListSerializer(instance=self.quiz).data
+        serializer_data = QuizListSerializer(instance=self.quiz).data
         res_data = json.loads(res.content)
-    
-        self.assertEqual([quiz_serializer_data], res_data)
+        self.assertEqual([serializer_data], res_data)
 
     def test_quiz_retrieve_view(self):
-
-        res = self.client.get(
-            path=reverse(
-                'cs_quiz.apps.quiz:quiz-retrieve', 
-                args=(str(self.quiz.id)))
-            )
+        res = self.client.get(path=f"/quiz/{self.quiz.id}/")
         self.assertEqual(200, res.status_code)
+        
+        serializer_data = QuizRetrieveSerializer(instance=self.quiz).data
+        res_data = json.loads(res.content)
+        self.assertEqual(serializer_data, res_data)
 
     def test_answer_create_view(self):
         res = self.client.post(
-            path=f"/quiz/answer/",
-            data={"quiz":f"{self.quiz.id}", "content":"테스트답변"},
-        )
+            path="/quiz/answers/",
+            data={"quiz":f"{self.quiz.id}", "content":"테스트답변"})
         self.assertEqual(201, res.status_code)
+        
+    def test_answer_list_view(self):
+        self.test_answer_create_view()
+        self.test_answer_create_view()
+        res = self.client.get(path=f"/quiz/{self.quiz.id}/answers/")
+        self.assertEqual(200, res.status_code)
+        
+        answer_arr = Answer.objects.filter(quiz=self.quiz.id)
+        test_data = {'quiz': answer_arr[0].quiz.content}
+        res_data = json.loads(res.content)
+        self.assertEqual(test_data['quiz'], res_data['quiz'])
+        self.assertEqual(2, res_data['count'])
--- cs_quiz/apps/quiz/urls.py
+++ cs_quiz/apps/quiz/urls.py
@@ -9,6 +9,6 @@ app_name = 'cs_quiz.apps.quiz'
 urlpatterns = [
     path("", QuizListView.as_view(), name="quiz-list"),
     path("<int:pk>/", QuizRetrieveView.as_view(), name="quiz-retrieve"),
-    path("answer/", AnswerCreateView.as_view(), name="answer-create"),
+    path("answers/", AnswerCreateView.as_view(), name="answer-create"),
     path("<int:pk>/answers/", AnswerListView.as_view(), name="answer-list"),
 ]
